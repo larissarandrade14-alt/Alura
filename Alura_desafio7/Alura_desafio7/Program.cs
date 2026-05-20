6900f54aa696dcb05b3911ababc908363f13a3c1
Larissa Andrade <larissarandrade14@gmail.com>
2026-05-20 14:55:20 -0300

    Desafio: conversor de moedas

--- Alura_desafio7/Alura_desafio7/Program.cs
+++ Alura_desafio7/Alura_desafio7/Program.cs
@@ -1,2 +1,15 @@
 ﻿//Desafio: conversor de moedas
+namespace Alura;
 
+public class Program
+{
+    public static void Main()
+    { 
+    Console.WriteLine("Digite o valor em real: ");
+       double real = double.Parse(Console.ReadLine());
+    Console.WriteLine("Digite a taxa de câmbio: ");
+       double cambio = double.Parse(Console.ReadLine());
+    double dolar = real / cambio;
+        Console.WriteLine("o valor em dólar é: " + dolar);
+    }
+}
