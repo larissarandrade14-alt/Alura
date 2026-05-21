987e4361e375fa05e8e3c0f28cc1125974b08a93
Larissa Andrade <larissarandrade14@gmail.com>
2026-05-21 11:55:11 -0300

    Modulo 1 do curso Alura C#: aplicando a Orientação a Objetos Primeira atividade

---
+++ Alura-classesemetodos-desafio1/Alura-classesemetodos-desafio1/Musica.cs
@@ -0,0 +1,25 @@
+﻿class Musica
+{
+    // Atributos da classe Musica
+    public string nome;
+    public string artista;
+    public int duracao;
+    public bool disponivelPlano;
+
+    public void ExibirInformacoes() // Método para exibir as informações da música
+    {
+        Console.WriteLine($"Nome da música: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração: {duracao} segundos");
+        if (disponivelPlano)
+        {
+            Console.WriteLine("Disponível para plano gratuito");
+        }
+        else
+        {
+            Console.WriteLine("Disponível apenas para plano pago");
+        }
+    }
+
+}
+
---
+++ Alura-classesemetodos-desafio1/Alura-classesemetodos-desafio1/Program.cs
@@ -0,0 +1,15 @@
+﻿Musica musica1 = new Musica();// Criação da primeira música (objeto)
+musica1.nome = "Vamos Fugir";
+musica1.artista = "Skank";
+musica1.duracao = 210;
+musica1.disponivelPlano = true;
+
+Musica musica2 = new Musica();
+musica2.nome = "Sutilmente";
+musica2.artista = "Skank";
+musica2.duracao =  240 ;
+musica2.disponivelPlano = false;
+
+musica1.ExibirInformacoes();// Exibe as informações da primeira música
+musica2.ExibirInformacoes();
+
