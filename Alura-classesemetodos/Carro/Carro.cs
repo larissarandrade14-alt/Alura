5a65c3a53d29a4c0d994ffdcc67cdf4d03b6b5c6
Larissa Andrade <larissarandrade14@gmail.com>
2026-05-22 14:48:13 -0300

    Desafio do carro - curso Alura classes e metodos

---
+++ Alura-classesemetodos/Carro/Carro.cs
@@ -0,0 +1,25 @@
+﻿class Carro
+{
+    public string fabricante;
+    public string modelo;
+    public int ano;
+    public int quantidadePortas;
+ 
+
+    public void exibirInformacoes()
+    {
+        Console.WriteLine($"Informações do carro: {fabricante}, {modelo}, {quantidadePortas} portas, {ano}");
+    }
+    public void Acelerar()
+    {
+        Console.WriteLine("O carro está acelerando...");
+    }
+    public void Frear()
+    {
+        Console.WriteLine("O carro está freando...");
+    }
+    public void Buzinar()
+    {
+        Console.WriteLine("BIBIBI");
+    }
+}
