e143f6d922db16b5924157141f3c6b0cd0ef064c
Larissa Andrade <larissarandrade14@gmail.com>
2026-05-22 14:14:05 -0300

    Reorganização de pastas, resolução do desafio 2 de conta bancaria classes e metodos

---
+++ Alura-classesemetodos/Alura-classesemetodos-desafio2/ContaBancaria.cs
@@ -0,0 +1,20 @@
+﻿class ContaBancaria
+{
+    public string titular;
+    public string senha;
+    public int numeroIdentificacao;
+    public double saldo;
+
+    public void ExibirDadosCompletos()
+    {
+        Console.WriteLine($"Titular: {titular}");
+        Console.WriteLine($"Número de Identificação: {numeroIdentificacao}");
+        Console.WriteLine($"Saldo: {saldo}");
+        Console.WriteLine($"Senha: {senha}");
+    }
+
+    public void ExibirSaldo()
+    {
+        Console.WriteLine($"\nO saldo na conta pertencente a {titular} é R${saldo}");
+    }
+}
---
+++ Alura-classesemetodos/Alura-classesemetodos-desafio2/ContaDados.cs
@@ -0,0 +1,9 @@
+﻿ContaBancaria contaNova = new ContaBancaria();
+contaNova.titular = "Maria";
+contaNova.senha = "123456";
+contaNova.saldo = 1000.00;
+contaNova.numeroIdentificacao = 12345;
+
+contaNova.ExibirDadosCompletos();
+
+contaNova.ExibirSaldo();
