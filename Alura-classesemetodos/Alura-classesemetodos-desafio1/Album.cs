f601635c02c62a5a74604adf2d9a0c06b4b5a5a7
Larissa Andrade <larissarandrade14@gmail.com>
2026-05-26 14:50:12 -0300

    Criação da classe Album

---
+++ Alura-classesemetodos/Alura-classesemetodos-desafio1/Album.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Channels;
+
+class Album
+{
+    private List<Musica> musicas = new List<Musica>();
+    public string Nome { get; set; }
+    public int DuracaoTotal => musicas.Sum(m => m.Duracao);
+
+    public void AdicionarMusica (Musica musica)
+    {
+        musicas.Add(musica);
+    }
+    public void ExibirMusicas()
+    {
+        Console.WriteLine($"Músicas do álbum {Nome}:\n");
+        foreach (var musica in musicas)
+        {
+            Console.WriteLine($"Musica: {musica.Nome}");
+        }
+        Console.WriteLine($"\nDuração total do álbum: {DuracaoTotal} segundos");
+    }
+   
+}
--- Alura-classesemetodos/Alura-classesemetodos-desafio1/Program.cs
+++ Alura-classesemetodos/Alura-classesemetodos-desafio1/Program.cs
@@ -1,18 +1,12 @@
-﻿Musica musica1 = new Musica();// Criação da primeira música (objeto)
-musica1.Nome = "Vamos Fugir";
-musica1.Artista = "Skank";
-musica1.Duracao = 210;
-musica1.DisponivelPlano = true;
-
+﻿Album albumSkank = new Album();
+albumSkank.Nome = "Calango";
+Musica musica1 = new Musica();
+musica1.Nome = "Jackie Tequila";
+musica1.Duracao = 250;
 Musica musica2 = new Musica();
-musica2.Nome = "Sutilmente";
-musica2.Artista = "Skank";
-musica2.Duracao =  240 ;
-musica2.DisponivelPlano = false;
+musica2.Nome = "Te ver";
+musica2.Duracao = 276;
 
-musica1.ExibirInformacoes();// Exibe as informações da primeira música
-Console.WriteLine(musica1.DescricaoResumida);
-
-musica2.ExibirInformacoes();
-Console.WriteLine(musica2.DescricaoResumida); // Acessa a propriedade de descrição resumida da segunda música
-
+albumSkank.AdicionarMusica(musica1);
+albumSkank.AdicionarMusica(musica2);
+albumSkank.ExibirMusicas();
