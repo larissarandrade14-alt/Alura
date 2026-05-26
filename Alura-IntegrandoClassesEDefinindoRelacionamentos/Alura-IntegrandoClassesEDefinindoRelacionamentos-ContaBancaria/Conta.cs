5e10dd4d1dacc4396c8b25921d129aa770316b3c
Larissa Andrade <larissarandrade14@gmail.com>
2026-05-26 15:47:35 -0300

    Integrando Classes e Definindo Relacionamentos - Conta Bancária

---
+++ Alura-IntegrandoClassesEDefinindoRelacionamentos/Alura-IntegrandoClassesEDefinindoRelacionamentos-ContaBancaria/Conta.cs
@@ -0,0 +1,27 @@
+﻿/* 1.Modelar uma classe Conta, que tenha como atributos uma classe Titular, além de informações da conta, como agência, 
+     número da conta, saldo e limite, bem como um método que devolva as informações da conta de forma detalhada.
+    2.Instanciar um objeto do tipo Conta e um do tipo Titular e mostrar as informações de Titular, a partir da Conta.*/
+
+public class Conta
+{
+    private class Titular
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+    }
+    public Titular titular = new Titular();
+    public int Agencia { get; set; }
+    public int NumeroConta { get; set; }
+    public double Saldo { get; set; }
+    public double Limite { get; set; }
+
+    public void ExibirInformacoes()
+    {
+        Console.WriteLine($"Nome do Titular: {titular.Nome}");
+        Console.WriteLine($"CPF: {titular.CPF}");
+        Console.WriteLine($"Agência: {Agencia}");
+        Console.WriteLine($"Número da Conta: {NumeroConta}");
+        Console.WriteLine($"Saldo: {Saldo}");
+        Console.WriteLine($"Limite: {Limite}");
+    }
+}
---
+++ Alura-IntegrandoClassesEDefinindoRelacionamentos/Alura-IntegrandoClassesEDefinindoRelacionamentos-ContaBancaria/Program.cs
@@ -0,0 +1,9 @@
+﻿Conta c = new Conta();
+c.titular.Nome = "Larissa Andrade";
+c.titular.CPF = "123.456.789-00";
+c.Saldo = 1000.00;
+c.NumeroConta = 12345;
+c.Agencia = 123;
+c.Limite = 500.00;
+
+c.ExibirInformacoes();
