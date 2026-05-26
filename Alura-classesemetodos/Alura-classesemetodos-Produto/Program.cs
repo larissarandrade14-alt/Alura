adab092bba3076054f24be19b4c19aaa9b6c47b0
Larissa Andrade <larissarandrade14@gmail.com>
2026-05-26 12:36:26 -0300

    Desafio: hora da prática

---
+++ Alura-classesemetodos/Alura-classesemetodos-Produto/Program.cs
@@ -0,0 +1,38 @@
+﻿class Produto
+{
+    public string Nome { get; set; }
+
+    private double _preco;
+    public double Preco
+    {
+        get => _preco;
+        set
+        {
+            if (_preco > 0)
+            {
+                _preco = value;
+            }
+            else
+            {
+                Console.WriteLine("O preço deve ser maior que zero.");
+            }
+        }
+    }
+
+    public string Marca { get; set; }
+
+    private int _estoque;
+    public int Estoque {
+        get => _estoque;
+        set
+        {
+            if (value < 0)
+            {
+                Console.WriteLine("O estoque não pode ser negativo.");
+            }
+            _estoque = value;
+        }
+    }
+
+
+}
