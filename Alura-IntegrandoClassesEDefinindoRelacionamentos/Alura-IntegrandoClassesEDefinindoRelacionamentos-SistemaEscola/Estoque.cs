ddfc3ec9bceb5bb35fbc1bd47f2e8e35e572b6f6
Larissa Andrade <larissarandrade14@gmail.com>
2026-05-27 11:07:38 -0300

    Desenvolver uma classe que represente um estoque de produtos, e que tenha as funcionalidades de adicionar novos produtos, e exibir todos os produtos no estoque.

---
+++ Alura-IntegrandoClassesEDefinindoRelacionamentos/Alura-IntegrandoClassesEDefinindoRelacionamentos-SistemaEscola/Estoque.cs
@@ -0,0 +1,80 @@
+﻿//Desenvolver uma classe que represente um estoque de produtos, e que tenha as funcionalidades de adicionar novos produtos, e exibir todos os produtos no estoque.
+using System.Security.Cryptography.X509Certificates;
+
+public class Produto
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    private int quantidade;
+    public int Quantidade
+    {
+        get => quantidade;
+        set
+        {
+            if (value < 0)
+            {
+                quantidade = 0;
+            }
+            else
+            {
+                quantidade = value;
+            }
+        }
+    }
+    public double preco;
+    public double Preco
+    {
+        get => preco;
+        set
+        {
+            if (value < 0)
+            {
+                preco = 0.10;
+            }
+            else
+            {
+                preco = value;
+            }
+        }
+    }
+    public string Marca { get; set; }
+    public string Descricao => $"ID: {Id} \nProduto: {Nome} \nQuantidade: {Quantidade} \nPreço: {Preco:C}";
+
+    public void ExibirInformacoes()
+    {
+        Console.WriteLine(Descricao);
+    }
+
+}
+public class Estoque
+{
+    public List<Produto> Produtos { get; set; } = new List<Produto>();
+
+    public void AdicionarProduto(Produto produto)
+    {
+        Produtos.Add(produto);
+        Console.WriteLine($"Produto {produto.Nome} adicionado ao estoque");
+    }
+
+    public void RemoverProduto(Produto produto)
+    {
+        Produtos.Remove(produto);
+        Console.WriteLine($"Produto {produto.Nome} removido do estoque");
+    }
+        public void ExibirTodosProdutos()
+    {
+        if (Produtos.Count == 0)
+        {
+            Console.WriteLine("Estoque vazio. Nenhum produto disponível");
+        }
+        else
+        {
+            Console.WriteLine("Lista de produtos no estoque:");
+            foreach (var produto in Produtos)
+            {
+                Console.WriteLine(produto.Descricao);
+            }
+        }
+
+    }
+}
---
+++ Alura-IntegrandoClassesEDefinindoRelacionamentos/Alura-IntegrandoClassesEDefinindoRelacionamentos-SistemaEscola/Program.cs
@@ -0,0 +1,60 @@
+﻿//Desenvolver uma classe que represente um estoque de produtos, e que tenha as funcionalidades de adicionar novos produtos, e exibir todos os produtos no estoque.
+Estoque produtos = new Estoque();   
+Produto xampu = new Produto()
+{
+    Id = 1,
+    Nome = "Xampu",
+    Quantidade = 10,
+    Preco = 15.50,
+    Marca = "Marca X"
+};
+Produto condicionador = new Produto()
+{
+    Id = 2,
+    Nome = "Condicionador",
+    Quantidade = 5,
+    Preco = 20.00,
+    Marca = "Marca Y"
+};
+
+produtos.AdicionarProduto(xampu);
+produtos.AdicionarProduto(condicionador);
+
+
+Produto sabonete = new Produto()
+{
+    Id = 3,
+    Nome = "Sabonete",
+    Quantidade = 20,
+    Preco = 5.00,
+    Marca = "Marca Z"
+};
+
+produtos.AdicionarProduto(sabonete);
+
+Produto escova = new Produto()
+{
+    Id = 4,
+    Nome = "Escova de Dente",
+    Quantidade = 15,
+    Preco = 8.00,
+    Marca = "Marca W"
+};
+produtos.AdicionarProduto(escova);
+produtos.RemoverProduto(xampu);
+
+Console.WriteLine("Digite o produto que você procura: ");
+string produtoProcurado = Console.ReadLine();
+if (produtoProcurado != null)
+{
+    var produtoEncontrado = produtos.Produtos.FirstOrDefault(p => p.Nome.Equals(produtoProcurado, StringComparison.OrdinalIgnoreCase));
+    if (produtoEncontrado != null)
+    {
+        Console.WriteLine("Produto encontrado:");
+        produtoEncontrado.ExibirInformacoes();
+    }
+    else
+    {
+        Console.WriteLine("Produto não encontrado no estoque.");
+    }
+}
