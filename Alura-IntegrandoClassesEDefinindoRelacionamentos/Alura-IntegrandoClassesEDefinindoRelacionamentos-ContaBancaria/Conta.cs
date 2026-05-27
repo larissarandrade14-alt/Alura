e87780a10c9d2b171efafa3a0e47c09bdea297da
Larissa Andrade <larissarandrade14@gmail.com>
2026-05-27 09:32:35 -0300

    Modelar uma classe Conta, que tenha como atributos uma classe Titular, além de informações da conta, como agência, número da conta, saldo e limite, bem como um método que devolva as informações da conta de forma detalhada. Instanciar um objeto do tipo Conta e um do tipo Titular e mostrar as informações de Titular, a partir da Conta.

--- Alura-IntegrandoClassesEDefinindoRelacionamentos/Alura-IntegrandoClassesEDefinindoRelacionamentos-ContaBancaria/Conta.cs
+++ Alura-IntegrandoClassesEDefinindoRelacionamentos/Alura-IntegrandoClassesEDefinindoRelacionamentos-ContaBancaria/Conta.cs
@@ -1,15 +1,15 @@
-﻿/* 1.Modelar uma classe Conta, que tenha como atributos uma classe Titular, além de informações da conta, como agência, 
+﻿/* 1/* 1.Modelar uma classe Conta, que tenha como atributos uma classe Titular, além de informações da conta, como agência, 
      número da conta, saldo e limite, bem como um método que devolva as informações da conta de forma detalhada.
     2.Instanciar um objeto do tipo Conta e um do tipo Titular e mostrar as informações de Titular, a partir da Conta.*/
 
+public class Titular
+{
+    public string Nome { get; set; }
+    public string CPF { get; set; }
+}
 public class Conta
 {
-    private class Titular
-    {
-        public string Nome { get; set; }
-        public string CPF { get; set; }
-    }
-    public Titular titular = new Titular();
+    public Titular Titular { get; set; }
     public int Agencia { get; set; }
     public int NumeroConta { get; set; }
     public double Saldo { get; set; }
@@ -17,11 +17,13 @@ public class Conta
 
     public void ExibirInformacoes()
     {
-        Console.WriteLine($"Nome do Titular: {titular.Nome}");
-        Console.WriteLine($"CPF: {titular.CPF}");
+        Console.WriteLine($"Nome do Titular: {Titular.Nome}");
+        Console.WriteLine($"CPF: {Titular.CPF}");
         Console.WriteLine($"Agência: {Agencia}");
         Console.WriteLine($"Número da Conta: {NumeroConta}");
         Console.WriteLine($"Saldo: {Saldo}");
         Console.WriteLine($"Limite: {Limite}");
     }
+
+
 }
--- Alura-IntegrandoClassesEDefinindoRelacionamentos/Alura-IntegrandoClassesEDefinindoRelacionamentos-ContaBancaria/Program.cs
+++ Alura-IntegrandoClassesEDefinindoRelacionamentos/Alura-IntegrandoClassesEDefinindoRelacionamentos-ContaBancaria/Program.cs
@@ -1,9 +1,12 @@
 ﻿Conta c = new Conta();
-c.titular.Nome = "Larissa Andrade";
-c.titular.CPF = "123.456.789-00";
+Titular t = new Titular();
+c.Titular = t;
+t.Nome = "Larissa Andrade";
+t.CPF = "123.456.789-00";
 c.Saldo = 1000.00;
 c.NumeroConta = 12345;
 c.Agencia = 123;
 c.Limite = 500.00;
 
 c.ExibirInformacoes();
+
