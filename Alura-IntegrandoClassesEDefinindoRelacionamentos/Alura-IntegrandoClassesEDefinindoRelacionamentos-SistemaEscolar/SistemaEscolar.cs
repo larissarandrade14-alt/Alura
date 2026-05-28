3133b421454d73a25154a8cabfd3d9c495b3eb69
Larissa Andrade <larissarandrade14@gmail.com>
2026-05-28 13:06:36 -0300

    SitemaEscolar - apenas classes

---
+++ Alura-IntegrandoClassesEDefinindoRelacionamentos/Alura-IntegrandoClassesEDefinindoRelacionamentos-SistemaEscolar/SistemaEscolar.cs
@@ -0,0 +1,21 @@
+﻿public class Aluno
+{
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+    public List<double> Nota { get; set; } = new List<double>();
+
+}
+
+public class Professor
+{
+    public string Nome { get; set; }
+    public List<string> DisciplinasLecionadas { get; set; } = new List<string>();
+   
+}
+
+public class Disciplina
+{
+    public string Nome { get; set; }
+    public List<Aluno> AlunosMatriculados { get; set; } = new List<Aluno>();
+
+}
