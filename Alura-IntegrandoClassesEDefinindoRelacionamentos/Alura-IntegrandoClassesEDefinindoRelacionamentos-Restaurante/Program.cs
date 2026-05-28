f939c6dad2db4632a4fa260e252482f30449a3f8
Larissa Andrade <larissarandrade14@gmail.com>
2026-05-28 14:10:20 -0300

    Modelar um sistema para um restaurante com classes como Restaurante, Mesa, Pedido e Cardapio. A classe Restaurante deve ter mesas que podem ser reservadas e um cardápio com itens que podem ser pedidos. Os pedidos podem estar associados a uma mesa.

---
+++ Alura-IntegrandoClassesEDefinindoRelacionamentos/Alura-IntegrandoClassesEDefinindoRelacionamentos-Restaurante/Program.cs
@@ -0,0 +1,23 @@
+﻿Restaurante restaurante = new Restaurante();
+
+restaurante.Mesas.Add(new Mesa()
+{
+    Numero = 1,
+    Ocupada = true
+});
+
+restaurante.Mesas.Add(new Mesa()
+{
+    Numero = 2,
+    Ocupada = false
+});
+
+restaurante.Mesas.Add(new Mesa()
+{
+    Numero = 3,
+    Ocupada = true
+});
+
+Pedidos pedidos = new Pedidos();
+
+pedidos.FazerPedido(restaurante);
---
+++ Alura-IntegrandoClassesEDefinindoRelacionamentos/Alura-IntegrandoClassesEDefinindoRelacionamentos-Restaurante/Restaurante.cs
@@ -0,0 +1,85 @@
+﻿public class Restaurante
+{
+    public List<Mesa> Mesas { get; set; } = new List<Mesa>();
+    public void MesaDisponivel(Mesa mesa)
+    {
+        if (mesa.Ocupada == false)
+            Console.WriteLine("Mesa disponível para reserva");
+        else Console.WriteLine("Mesa ocupada, selecione outra mesa para reserva");
+    }
+    public List<string> Cardapio { get; set; } = new List<string> { "Pizza", "Hambúrguer", "Salada", "Sopa", "Frango Assado" };
+}
+public class Mesa
+{
+    public int Numero { get; set; }
+    public bool Ocupada { get; set; }
+
+}
+public class Pedidos
+{
+    public void FazerPedido(Restaurante restaurante)
+    {
+        int numeroMesa;
+
+        while (true)
+        {
+            Console.WriteLine("Digite o número da mesa:");
+
+            bool numeroValido =
+                int.TryParse(Console.ReadLine(), out numeroMesa);
+
+            if (numeroValido)
+                break;
+
+            Console.WriteLine("Digite apenas números.");
+        }
+
+        Mesa mesaEncontrada = restaurante.Mesas.FirstOrDefault(m => m.Numero == numeroMesa);
+        if (mesaEncontrada == null)
+        {
+            Console.WriteLine("Mesa não encontrada. Pedido não realizado.");
+            return;
+        }
+        if (mesaEncontrada.Ocupada)
+        {
+            Console.WriteLine("=== CARDÁPIO ===");
+
+            foreach (var item in restaurante.Cardapio)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("\nRealize seu pedido: ");
+            string pedidoRealizado = Console.ReadLine();
+            string pedidoCardapio = restaurante.Cardapio.FirstOrDefault(p => p.Equals(pedidoRealizado, StringComparison.OrdinalIgnoreCase));
+            switch (pedidoCardapio)
+            {
+
+
+
+                case "Pizza":
+                    Console.WriteLine("Pedido de Pizza realizado com sucesso!");
+                    break;
+                case "Hambúrguer":
+                    Console.WriteLine("Pedido de Hambúrguer realizado com sucesso!");
+                    break;
+                case "Salada":
+                    Console.WriteLine("Pedido de Salada realizado com sucesso!");
+                    break;
+                case "Sopa":
+                    Console.WriteLine("Pedido de Sopa realizado com sucesso!");
+                    break;
+                case "Frango Assado":
+                    Console.WriteLine("Pedido de Frango Assado realizado com sucesso!");
+                    break;
+                default:
+                    Console.WriteLine("Item não encontrado no cardápio.");
+                    break;
+
+            }
+        }
+        else
+        {
+            Console.WriteLine("Mesa livre, não há como fazer pedidos.");
+        }
+    }
+}
