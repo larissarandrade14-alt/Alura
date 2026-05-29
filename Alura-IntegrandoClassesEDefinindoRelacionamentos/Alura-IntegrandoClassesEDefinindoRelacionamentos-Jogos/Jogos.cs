4de11c795575ebc51420ff35a0b96549c42e2e2b
Larissa Andrade <larissarandrade14@gmail.com>
2026-05-29 14:36:48 -0300

    Desenvolver uma classe que representa um catálogo de jogos, com uma lista de Jogos e métodos para manipular essa lista, bem como um construtor que faça sua inicialização.

---
+++ Alura-IntegrandoClassesEDefinindoRelacionamentos/Alura-IntegrandoClassesEDefinindoRelacionamentos-Jogos/Jogos.cs
@@ -0,0 +1,39 @@
+﻿//Desenvolver uma classe que representa um catálogo de jogos, com uma lista de
+//Jogos e métodos para manipular essa lista, bem como um construtor que faça sua inicialização.
+public class Jogos
+{
+    public Jogos(string nome, int quantidadeJogadores, string dificuldade)
+    {
+        Nome = nome;
+        QuantidadeJogadores = quantidadeJogadores;
+        Dificuldade = dificuldade;
+    }
+
+    public string Nome { get; }
+    public int QuantidadeJogadores { get; }
+    public string Dificuldade { get; }
+    public string Informacoes => $"{Nome} - {QuantidadeJogadores} jogadores - Dificuldade: {Dificuldade}"; 
+}
+public class CatalogoJogos
+{
+    public List<Jogos> CatalogodeJogos = new List<Jogos>();
+    public void AdicionarJogo(Jogos jogo)
+    {
+        CatalogodeJogos.Add(jogo);
+    }
+    public void ExibirCatalogo()
+    {
+        Console.WriteLine("====== CATÁLOGO DE JOGOS ======\n");
+        if (!CatalogodeJogos.Any())
+        {
+            Console.WriteLine("Nenhum jogo cadastrado.");
+            return;
+        }
+
+        foreach (var jogo in CatalogodeJogos)
+        {
+            Console.WriteLine(jogo.Informacoes);
+        }
+
+    }
+}
---
+++ Alura-IntegrandoClassesEDefinindoRelacionamentos/Alura-IntegrandoClassesEDefinindoRelacionamentos-Jogos/Program.cs
@@ -0,0 +1,10 @@
+﻿CatalogoJogos cj = new CatalogoJogos();
+Jogos jogo1 = new Jogos("The Legend of Zelda: Breath of the Wild", 1, "Médio");
+Jogos jogo2 = new Jogos("Among Us", 4, "Fácil");
+Jogos jogo3 = new Jogos("Dark Souls III", 1, "Difícil");
+
+cj.AdicionarJogo(jogo1);
+cj.AdicionarJogo(jogo2);
+cj.AdicionarJogo(jogo3);
+
+cj.ExibirCatalogo();
