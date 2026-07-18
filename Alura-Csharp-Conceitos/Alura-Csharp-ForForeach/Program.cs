aaedd023dab30f8751b7b57b4779b339d3bd200f
Larissa Andrade <larissarandrade14@gmail.com>
2026-07-18 12:02:40 -0300

    Fixação de for e foreach

--- Alura-Csharp-Conceitos/Alura-Csharp-ForForeach/Program.cs
+++ Alura-Csharp-Conceitos/Alura-Csharp-ForForeach/Program.cs
@@ -16,7 +16,34 @@ void MostrarNumeros()
         Console.WriteLine(numeros[i]);
     }
 }
-Console.WriteLine($"Usando o foreach:");
+void NumerosParesForeach()
+{
+    foreach (int numero in numeros)
+    {
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine(numero);
+        }
+    }
+}
+void NumerosParesFor()
+{
+for (int i=0; i<numeros.Count; i++)
+    {
+        if (numeros[i] % 2 == 0)
+        {
+            Console.WriteLine(numeros[i]);
+        }
+    }
+}
+
+Console.WriteLine($"Usando o foreach:\n");
+Console.WriteLine("Todos os números: ");
 ImprimirNumeros();
-Console.WriteLine("Usando o for:");
+Console.WriteLine("Apenas os números pares: ");
+NumerosParesForeach();
+Console.WriteLine("\nUsando o for:\n");
+Console.WriteLine("Todos os números: ");
 MostrarNumeros();
+Console.WriteLine("Apenas os números pares: ");
+NumerosParesFor();
