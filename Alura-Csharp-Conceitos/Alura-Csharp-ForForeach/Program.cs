8a1c2b77e70dfc181a54c6cf6976c7ba31550d14
Larissa Andrade <larissarandrade14@gmail.com>
2026-07-18 10:47:51 -0300

    For e Foreach - fixação

---
+++ Alura-Csharp-Conceitos/Alura-Csharp-ForForeach/Program.cs
@@ -0,0 +1,22 @@
+﻿List<int> numeros = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+void ImprimirNumeros()
+{
+    foreach (int numero in numeros)
+    {
+       
+        Console.WriteLine(numero);
+    }
+}
+void MostrarNumeros()
+{
+    for (int i = 0; i < numeros.Count; i++)
+    {
+        
+        Console.WriteLine(numeros[i]);
+    }
+}
+Console.WriteLine($"Usando o foreach:");
+ImprimirNumeros();
+Console.WriteLine("Usando o for:");
+MostrarNumeros();
